a00e9c4cd52dc85256148febf8c135fada71ec4d
Legends <78807240+xlegends@users.noreply.github.com>
2023-01-06 07:49:22 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var blocked = `
+    <link href="https://cdn.jsdelivr.net/gh/proudparrot2/cdn/fire.css" rel="stylesheet" type="text/css"/>
+
+  <style>
+  
+    body {
+      transform: translate(-50%, -50%);
+      text-align: center;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+    }
+      
+    h1 { font-size: 25px;}
+    .btn { width: 75%; font-weight: bold; font-size: 13px; margin: 5px;}
+  
+  </style>
+  <h1>This embed was blocked.</h1> <br>
+  <button class="btn btn-secondary" onclick="UnframedRedirect()">Go to the original page</button>
+`
+
+if (window.self != window.top) {
+  if (!document.referrer.includes(self.location)) {
+    document.querySelector("html").innerHTML = blocked
+  }
+}
+
+function UnframedRedirect() {
+  open(self.location)
+}
